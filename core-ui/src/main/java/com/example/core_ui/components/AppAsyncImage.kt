ac27d7af5d8de615dcfacdf1bac9c83a34aa22ad
Lucas Perera Montanari <lucasmontanari.97@gmail.com>
2023-01-31 17:43:06 -0300

    Add PodcastDetails Image and title

---
+++ core-ui/src/main/java/com/example/core_ui/components/AppAsyncImage.kt
@@ -0,0 +1,21 @@
+package com.example.core_ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import coil.compose.SubcomposeAsyncImage
+import com.example.core_ui.theme.Gray
+
+@Composable
+fun AppAsyncImage(
+    imageUrl: String,
+    contentDescription: String,
+    modifier: Modifier = Modifier
+) {
+    SubcomposeAsyncImage(
+        model = imageUrl,
+        modifier = modifier
+            .background(Gray),
+        contentDescription = contentDescription
+    )
+}
---
+++ podcast-details-ui/src/main/java/com/example/podcast_details_ui/screens/podcastDetails/components/PodcastDetailsContent.kt
@@ -0,0 +1,126 @@
+package com.example.podcast_details_ui.screens.podcastDetails.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.core_ui.components.AppAsyncImage
+import com.example.core_ui.theme.Green
+import com.example.core_ui.theme.PodcastAppTheme
+import com.example.core_ui.theme.Typography
+import com.example.podcast_details_domain.models.Episode
+import com.example.podcast_details_domain.models.Genre
+import com.example.podcast_details_domain.models.PodcastDetails
+import com.example.podcast_details_ui.screens.podcastDetails.PodcastDetailsViewModel
+import com.example.podcast_details_ui.screens.podcastDetails.ScreenContent
+import kotlin.random.Random
+
+@Composable
+fun PodcastDetailsContent(
+    podcastDetails: PodcastDetails,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .padding(horizontal = 10.dp)
+    ) {
+        AppAsyncImage(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(360.dp)
+                .clip(
+                    RoundedCornerShape(20)
+                ),
+            imageUrl = podcastDetails.imageUrl ?: "",
+            contentDescription = podcastDetails.name ?: ""
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+        PodcastTitle(
+            podcastName = podcastDetails.name ?: "",
+            authorName = podcastDetails.authorName
+        )
+    }
+}
+
+@Composable
+fun PodcastTitle(
+    podcastName: String,
+    authorName: String?
+) {
+    Column {
+        authorName?.let {
+            Text(
+                text = it,
+                style = MaterialTheme.typography.titleSmall,
+                color = Green
+            )
+        }
+        Text(
+            text = podcastName,
+            style = MaterialTheme.typography.titleLarge,
+        )
+    }
+}
+
+
+@Preview
+@Composable
+fun Preview_PodcastDetailsContent() {
+    PodcastAppTheme {
+        PodcastDetailsContent(
+            podcastDetails = mockPodcast,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(20.dp)
+        )
+    }
+}
+
+private val mockPodcast = PodcastDetails(
+    uuid = "d682a935-ad2d-46ee-a0ac-139198b83bcc",
+    name = "This American Life",
+    description = "This American Life is a weekly public radio show, heard by 2.2 million people on more than 500 stations. Another 2.5 million people download the weekly podcast. It is hosted by Ira Glass, produced in collaboration with Chicago Public Media, delivered to stations by PRX The Public Radio Exchange, and has won all of the major broadcasting awards.",
+    imageUrl = "https://files.thisamericanlife.org/sites/all/themes/thislife/img/tal-name-1400x1400.png",
+    genres = listOf(
+        Genre(
+            genreText = "fiction",
+            genreRemoteEnumString = "PODCASTSERIES_FICTION"
+        )
+    ),
+    websiteUrl = null,
+    authorName = "Juan Menguez",
+    episodes = listOf(
+        mockEpisode(),
+        mockEpisode().copy(
+            uuid = "eb9d1c8f-58a4-4adb-a3d3-6bca573d31a3",
+            name = "Case #2 Britney"
+        ),
+        mockEpisode().copy(
+            uuid = "eb9d1c8f-58a4-4adb-a3d3-6bca573d31a3",
+            name = "Case #2 Britney"
+        ),
+    )
+)
+
+private fun mockEpisode(
+    watched: Boolean = Random.nextBoolean()
+) = Episode(
+    podcastUuid = "8c9998d7-7114-4514-ab17-1a0ad05f73fc",
+    uuid = "eb9d1c8f-58a4-4adb-a3d3-6bca573d31a2",
+    name = "Case #1 Britney",
+    imageUrl = null,
+    datePublished = 1432958400,
+    description = "The Case:  Andrea's a writer no one reads. Then she makes a shocking discovery. The Facts:  Mystery Show is produced by myself, Alex Blumberg, Melinda Shopsin and Eric Mennel. Producing help from Chris Neary and Wendy Dorr. Eli Horowitz is contributing ed",
+    audioUrl = "https://traffic.megaphone.fm/GLT5025099642.mp3?updated=1511216902",
+    duration = 2964,
+    seasonNumber = null,
+    episodeNumber = null,
+    watched = watched,
+    timeWatched = if (watched) (0..2964).random() else 0
+)
