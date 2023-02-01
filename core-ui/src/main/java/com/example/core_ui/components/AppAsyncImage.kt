0a929acf1037da163fd88102c896758c0cc67b19
Lucas Perera Montanari <lucasmontanari.97@gmail.com>
2023-02-01 10:57:58 -0300

    Fix image content scale

--- core-ui/src/main/java/com/example/core_ui/components/AppAsyncImage.kt
+++ core-ui/src/main/java/com/example/core_ui/components/AppAsyncImage.kt
@@ -3,6 +3,7 @@ package com.example.core_ui.components
 import androidx.compose.foundation.background
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import coil.compose.SubcomposeAsyncImage
 import com.example.core_ui.theme.Gray
 
@@ -16,6 +17,8 @@ fun AppAsyncImage(
         model = imageUrl,
         modifier = modifier
             .background(Gray),
-        contentDescription = contentDescription
+        contentDescription = contentDescription,
+        contentScale = ContentScale.Crop
+
     )
 }
