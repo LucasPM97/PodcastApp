4ba6f7693fda4c3c3681eb570b900cb3b337595e
Lucas Perera Montanari <lucasmontanari.97@gmail.com>
2023-02-03 14:42:43 -0300

    Move ScrollToEpisodeList logic to its component

--- podcast-details-ui/src/main/java/com/example/podcast_details_ui/screens/podcastDetails/components/ScrollToEpisodesButton.kt
+++ podcast-details-ui/src/main/java/com/example/podcast_details_ui/screens/podcastDetails/components/ScrollToEpisodesButton.kt
@@ -1,29 +1,41 @@
 package com.example.podcast_details_ui.screens.podcastDetails.components
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.ScrollableState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import kotlinx.coroutines.launch
 
 @Composable
 fun ScrollToEpisodesButton(
     modifier: Modifier = Modifier,
-    onClick: () -> Unit
+    scrollState: LazyListState? = null,
+    firstEpisodeItemIndex: Int? = null,
 ) {
+    val coroutineScope = rememberCoroutineScope()
+    fun handleScrollToEpisodeList() {
+        coroutineScope.launch {
+            scrollState?.animateScrollToItem(index = firstEpisodeItemIndex ?: 0)
+        }
+    }
+
     Row(
         modifier,
         horizontalArrangement = Arrangement.Center
     ) {
         Row(
             modifier = Modifier.clickable {
-                onClick()
+                handleScrollToEpisodeList()
             },
             verticalAlignment = Alignment.CenterVertically,
         ) {
