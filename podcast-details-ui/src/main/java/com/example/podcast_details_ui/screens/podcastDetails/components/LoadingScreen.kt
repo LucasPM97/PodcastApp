50f8894161237960ff7d325b4e58a3506351fdf2
Lucas Perera Montanari <lucasmontanari.97@gmail.com>
2023-02-03 16:34:20 -0300

    Add LoadingScreen

---
+++ podcast-details-ui/src/main/java/com/example/podcast_details_ui/screens/podcastDetails/components/LoadingScreen.kt
@@ -0,0 +1,25 @@
+package com.example.podcast_details_ui.screens.podcastDetails.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LoadingScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Column {
+            CircularProgressIndicator(
+                color = MaterialTheme.colorScheme.secondary,
+            )
+        }
+    }
+}
