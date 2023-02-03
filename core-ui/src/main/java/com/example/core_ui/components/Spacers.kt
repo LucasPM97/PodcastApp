016f8730cf8afe52f4f40433653f8b4666b9a51a
Lucas Perera Montanari <lucasmontanari.97@gmail.com>
2023-02-03 14:45:18 -0300

    Move PodcastDetailsContent Spacer to Core-UI

---
+++ core-ui/src/main/java/com/example/core_ui/components/Spacers.kt
@@ -0,0 +1,12 @@
+package com.example.core_ui.components
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SpaceVertical20() {
+    Spacer(modifier = Modifier.height(20.dp))
+}
