7c6fb2ac37ec95eb95e8e82eb854969cdd36794e
Lucas Perera Montanari <lucasmontanari.97@gmail.com>
2023-02-09 13:36:40 -0300

    Able MediaSession add and play new MediaItems

--- podcast-player-ui/src/main/java/com/example/podcast_player_ui/data/services/PodcastPlayerService.kt
+++ podcast-player-ui/src/main/java/com/example/podcast_player_ui/data/services/PodcastPlayerService.kt
@@ -2,11 +2,11 @@ package com.example.podcast_player_ui.data.services
 
 import android.content.ComponentName
 import android.content.Context
+import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
-import androidx.media3.session.MediaController
-import androidx.media3.session.MediaSession
-import androidx.media3.session.MediaSessionService
-import androidx.media3.session.SessionToken
+import androidx.media3.session.*
+import com.google.common.util.concurrent.Futures
+import com.google.common.util.concurrent.ListenableFuture
 import kotlinx.coroutines.guava.await
 import org.koin.android.ext.android.inject
 
@@ -18,13 +18,23 @@ class PodcastPlayerService : MediaSessionService() {
     override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? =
         mediaSession
 
-
     override fun onDestroy() {
         mediaSession.run {
             player.release()
             release()
         }
         super.onDestroy()
+    }
+
+    class CustomMediaSessionCallback() : MediaSession.Callback {
+        override fun onAddMediaItems(
+            mediaSession: MediaSession,
+            controller: MediaSession.ControllerInfo,
+            mediaItems: MutableList<MediaItem>
+        ): ListenableFuture<MutableList<MediaItem>> {
+            val updatedMediaItems = mediaItems.map { it.buildUpon().setUri(it.mediaId).build() }.toMutableList()
+            return Futures.immediateFuture(updatedMediaItems)
+        }
     }
 }
 
--- podcast-player-ui/src/main/java/com/example/podcast_player_ui/di/PlayerModule.kt
+++ podcast-player-ui/src/main/java/com/example/podcast_player_ui/di/PlayerModule.kt
@@ -1,10 +1,10 @@
-package com.example.podcast_player_data.di
+package com.example.podcast_player_ui.di
 
 import androidx.media3.common.AudioAttributes
-import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.session.MediaSession
+import com.example.podcast_player_ui.data.services.PodcastPlayerService
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
@@ -13,14 +13,12 @@ val playerModule = module {
         ExoPlayer.Builder(androidContext())
             .setAudioAttributes(AudioAttributes.DEFAULT, true)
             .setHandleAudioBecomingNoisy(true)
-            .build().apply {
-                val mediaItem =
-                    MediaItem.fromUri("https://traffic.megaphone.fm/GLT5025099642.mp3?updated=1511216902")
-                setMediaItem(mediaItem)
-            }
+            .build()
     }
     single {
+        val customCallback = PodcastPlayerService.CustomMediaSessionCallback()
         MediaSession.Builder(androidContext(), get())
+            .setCallback(customCallback)
             .build()
     }
 }
