539f107c978283ab3b4eaf1482fe58c9961fedc8
Lucas Perera Montanari <lucasmontanari.97@gmail.com>
2023-02-14 12:47:29 -0300

    Clean Player height animation

--- podcast-player-ui/src/main/java/com/example/podcast_player_ui/components/PlayerDraggableBox.kt
+++ podcast-player-ui/src/main/java/com/example/podcast_player_ui/components/PlayerDraggableBox.kt
@@ -1,18 +1,25 @@
 package com.example.podcast_player_ui.components
 
-import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.gestures.rememberDraggableState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.example.podcast_player_ui.extensions.componentSizeHeight
+import com.example.core_ui.theme.DarkGray
 import com.example.podcast_player_ui.models.ComponentSize
 import kotlinx.coroutines.launch
 
@@ -22,8 +29,7 @@ fun PlayerDraggableBox(
     modifier: Modifier = Modifier,
     screenHeight: Dp,
     onComponentSizeChanged: (ComponentSize) -> Unit = {},
-    onHeightChanged: (boxHeightInPx: Int) -> Unit = {},
-    content: @Composable BoxScope.() -> Unit = {}
+    content: @Composable BoxScope.(screenFilled: Float) -> Unit = {}
 ) {
 
     val coroutineScope = rememberCoroutineScope()
@@ -41,45 +47,81 @@ fun PlayerDraggableBox(
         mutableStateOf(componentSize)
     }
 
-    Box(
-        modifier = modifier
-            .componentSizeHeight(
-                componentSize,
-                dynamicHeight = if (onDragging) dynamicBoxHeightInDp
-                else 0.dp
-            )
-            .onSizeChanged {
-                if (!onDragging) {
-                    boxHeightInPx = it.height
-                }
-                onHeightChanged(it.height)
+
+    fun calculateHeight(componentSize: ComponentSize): Dp {
+        val height = if (onDragging) {
+            dynamicBoxHeightInDp
+        } else {
+            when (componentSize) {
+                ComponentSize.None -> 0.dp
+                ComponentSize.Small -> ROW_PLAYER_HEIGHT
+                ComponentSize.FullScreen -> screenHeight
             }
-            .draggable(
-                orientation = Orientation.Vertical,
-                state = rememberDraggableState { delta ->
-                    coroutineScope.launch {
-                        nextComponentSize = calculateNextComponentSize(
-                            currentComponentSize = componentSize,
-                            boxHeight = dynamicBoxHeightInDp,
-                            screenHeight = screenHeight,
-                            oldOffset = offsetY,
-                            newOffset = offsetY + delta,
-                        )
-                        offsetY += delta
-                    }
-                },
-                onDragStarted = {
-                    onDragging = true
-                    offsetY = 0f
-                },
-                onDragStopped = {
-                    onDragging = false
-                    onComponentSizeChanged(nextComponentSize)
-                }
-            )
-            .animateContentSize()
+        }
+        return height
+    }
+
+    val transition = updateTransition(
+        targetState = componentSize,
+        label = "component size"
+    )
+    val animatedHeight by transition.animateDp(
+        targetValueByState = {
+            calculateHeight(it)
+        },
+        transitionSpec = { tween(durationMillis = 500) },
+        label = "animate height"
+    )
+
+    val percentageOfScreenFilled = (animatedHeight * 100) / screenHeight
+    val backgroundColor by animateColorAsState(
+        targetValue = when {
+            percentageOfScreenFilled < 20f -> DarkGray
+            percentageOfScreenFilled < 40f -> Color(0xFF1D2125)
+            percentageOfScreenFilled < 60f -> Color(0xFF191D20)
+            percentageOfScreenFilled < 85f -> Color(0xFF15181A)
+            else -> MaterialTheme.colorScheme.primary
+        },
+        animationSpec = tween(durationMillis = 150)
+    )
+
+    Surface(
+        color = backgroundColor
     ) {
-        content()
+        Box(
+            modifier = modifier
+                .height(animatedHeight)
+                .onSizeChanged {
+                    if (!onDragging) {
+                        boxHeightInPx = it.height
+                    }
+                }
+                .draggable(
+                    orientation = Orientation.Vertical,
+                    state = rememberDraggableState { delta ->
+                        coroutineScope.launch {
+                            nextComponentSize = calculateNextComponentSize(
+                                currentComponentSize = componentSize,
+                                boxHeight = dynamicBoxHeightInDp,
+                                screenHeight = screenHeight,
+                                oldOffset = offsetY,
+                                newOffset = offsetY + delta,
+                            )
+                            offsetY += delta
+                        }
+                    },
+                    onDragStarted = {
+                        onDragging = true
+                        offsetY = 0f
+                    },
+                    onDragStopped = {
+                        onDragging = false
+                        onComponentSizeChanged(nextComponentSize)
+                    }
+                )
+        ) {
+            content(percentageOfScreenFilled)
+        }
     }
 }
 
