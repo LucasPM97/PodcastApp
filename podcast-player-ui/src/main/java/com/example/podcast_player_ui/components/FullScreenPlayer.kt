e68b433d7d93b60abf7bdf6a615a63777dadd251
Lucas Perera Montanari <lucasmontanari.97@gmail.com>
2023-02-15 13:47:18 -0300

    Show Timer bar with Duration and Current position

--- podcast-player-ui/src/main/java/com/example/podcast_player_ui/components/FullScreenPlayer.kt
+++ podcast-player-ui/src/main/java/com/example/podcast_player_ui/components/FullScreenPlayer.kt
@@ -1,12 +1,15 @@
 package com.example.podcast_player_ui.components
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.media3.session.MediaController
+import com.example.core.extensions.durationText
 import com.example.core.mocks.mockEpisode
 import com.example.core.models.Episode
 import com.example.core_ui.components.AppAsyncImage
@@ -15,11 +18,13 @@ import com.example.core_ui.extensions.roundedRectangle
 import com.example.core_ui.theme.PodcastAppTheme
 import com.example.podcast_player_ui.components.fullScreenPlayer.CollapseButton
 import com.example.podcast_player_ui.components.fullScreenPlayer.PlayerButtons
+import com.example.podcast_player_ui.components.fullScreenPlayer.PlayerTimeBar
 import com.example.podcast_player_ui.components.fullScreenPlayer.TitleWithFav
 
 @Composable
 fun FullScreenPlayer(
     episode: Episode?,
+    mediaController: MediaController?,
     mediaControllerState: PlayerState?,
     modifier: Modifier = Modifier,
     collapsePlayer: () -> Unit = {},
@@ -49,6 +54,12 @@ fun FullScreenPlayer(
             SpacerVertical20()
             TitleWithFav(episode)
             SpacerVertical20()
+            PlayerTimeBar(
+                mediaController,
+                isPlaying = mediaControllerState?.isPlaying ?: false,
+                episode,
+
+                )
             SpacerVertical20()
             PlayerButtons(
                 isPlaying = mediaControllerState?.isPlaying ?: false,
@@ -65,6 +76,7 @@ private fun Preview_FullScreenPlayer() {
     PodcastAppTheme {
         FullScreenPlayer(
             episode = mockEpisode(),
+            mediaController = null,
             mediaControllerState = null,
             modifier = Modifier
                 .fillMaxSize()
